e3bc88e50fdef504cda0fcd5e3c68a78a5209671
mohikhsan <mohammadikhsan07@gmail.com>
2017-04-19 22:34:02 +0800

    Added load_db

--- data.py
+++ data.py
@@ -3,22 +3,69 @@ Load training and train data from image files
 """
 import os
 import numpy as np
-from skimage import imsave, imread
+from skimage.io import imsave, imread
 
 db_path = 'db/'
+img_height = 480
+img_width = 640
 
-roi_width = 300
-roi_height = 200
-roi_x = 100
-roi_y = 100
+def create_db(roi=None):
+    """
+    Create db files from raw images
 
-def create_db():
-    subjects = os.listdir(db_path)
-    print(subjects)
+    Parameters:
+        -roi: region of interest [y,height,x,width]
+    """
+    if roi is not None:
+        roi_y = roi[0]
+        roi_height = roi[1]
+        roi_x = roi[2]
+        roi_width = roi[3]
+    else:
+        roi_y = 0
+        roi_height = img_height
+        roi_x = 0
+        roi_width = img_width
+
+    subjects = [name for name in os.listdir(db_path) if os.path.isdir(os.path.join(db_path,name))]
+
     num_img = 0
+    db_structure = []
     for subject in subjects:
         subject_path = os.path.join(db_path, subject)
         num_subject_files = len([fname for fname in os.listdir(subject_path) if os.path.isfile(os.path.join(subject_path, fname))])
         num_img += num_subject_files
-        print("Subject: " + subject + " Files: " + str(num_subject_files))
-    print(num_img)
+        db_structure.append([subject,num_subject_files])
+
+    imgs = np.ndarray((num_img, roi_height, roi_width), dtype=np.uint8)
+
+    idx = 0
+    print("Loading image...")
+    for subject in subjects:
+        subject_path = os.path.join(db_path, subject)
+        images = os.listdir(subject_path)
+        for image_fname in images:
+            if image_fname.endswith('.png'):
+                img = imread(os.path.join(subject_path, image_fname), as_grey=True)
+                if roi is not None:
+                    img = np.array([img[roi_y:roi_y+roi_height,roi_x:roi_x+roi_width]])
+                else:
+                    img = np.array([img])
+                imgs[idx] = img
+
+                if idx % 100 == 0:
+                    print('Completed {0}/{1} images'.format(idx, num_img))
+
+                idx += 1
+
+    print("Loading complete.")
+
+    np.save('img_db.npy',imgs)
+
+    print("Images saved to img_db.npy")
+
+def load_db():
+    try:
+        return np.load('img_db.npy')
+    except:
+        return None
--- models.py
+++ models.py
@@ -1,3 +1,18 @@
 """
 Define deep learning models
 """
+
+from keras.models import Model
+from keras.layers import (
+    Input, Conv2D, MaxPooling2D, UpSampling2D, Activation,
+    Reshape, BatchNormalization, Concatenate)
+
+
+def get_unet(input_shape, num_labels):
+    print('Normal U-Net')
+
+def get_unet_batchnorm(input_shape, num_labels):
+    print('U-Net with Batch Normalization')
+
+def get_conv_batchnorm_block():
+    print('Create convolution block with batch normalization')
